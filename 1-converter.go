2f6a6a3d39b380260d9cadda9d050a3a9da3b8a1
Alikhan <alikhanmamilov@yandex.ru>
2025-07-08 15:44:01 +0300

    2-задание

--- 1-converter.go
+++ 1-converter.go
@@ -1,7 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 	const USDToEUR = 0.852 
 	const USDToRUB = 78.72
 	const EURToRUB = USDToRUB/USDToEUR
+
+func main() {
+	usrInp := userInput()
+	fmt.Println(usrInp)
+}
+
+func userInput() int64{
+	var usrInput int64
+	fmt.Scan(&usrInput)
+	return usrInput
+}
+
+func converter(int64, string, string) int64 {
+//return 
 }
