f93b7bc32b0fe0519b4c7ca4992da33cdb0052ee
Alikhan <alikhanmamilov@yandex.ru>
2025-11-07 21:23:26 +0300

    1.1-push testing

--- api/api_test.go
+++ api/api_test.go
@@ -12,8 +12,11 @@ func TestCreateBin(t *testing.T) {
 			if err != nil {
 				t.Error("Не удалось найти ENV файл")
 			}
-	var filesmanager files.FilesManager = &files.Files{}
-	var apimanager api.ApiManager = &api.ApiStruct{}
+			
+			var filesmanager files.FilesManager = &files.Files{}
+			var apimanager api.ApiManager = &api.ApiStruct{}
+			
+	key := apimanager.GetKey()
 	data, err := filesmanager.ReadAnyFile("../file.json")
 	if err != nil {
 		t.Error(err)
@@ -27,4 +30,5 @@ func TestCreateBin(t *testing.T) {
 	if id == nil {
 		t.Errorf("ожидалась длина ответа больше 0, получили %s", id)
 	}
+	apimanager.Delete(id.Metadata.ID, key)
 }
