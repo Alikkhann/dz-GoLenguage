ee9f767e480b1c0c5b27f0b2055077185ab3d3f6
Alikhan <alikhanmamilov@yandex.ru>
2025-11-09 21:20:54 +0300

    2-finish

--- api/api_test.go
+++ api/api_test.go
@@ -14,13 +14,17 @@ func generateUniqueId() string {
 	return fmt.Sprintf("test-%d", time.Now().UnixNano())
 }
 
+func generateUniqueName() string {
+	return fmt.Sprintf("updated-test-%d", time.Now().UnixNano())
+}
+
 	var testData = bins.BinList{
 		Bins: []bins.Bin{
 			{
 				Id: generateUniqueId(),
       	Private: true,
      	 	CreatedAt: time.Now(),
-     	  Name: "test-create-bin",
+     	  Name: generateUniqueId(),
 			},
 		},
 	}
@@ -110,8 +114,18 @@ func TestPutBin(t *testing.T) {
 			if id == nil {
 				t.Errorf("ожидалась длина ответа больше 0, получили %s", id)
 			}
-				
-	testDataPut := testData
+
+	testDataPut := bins.BinList{
+		Bins: []bins.Bin{
+			{
+				Id: generateUniqueId(),
+      	Private: true,
+     	 	CreatedAt: time.Now(),
+     	  Name: generateUniqueName(),
+			},
+		},
+	}
+	
 	dataPut, err := json.Marshal(testDataPut)
 			if err != nil {
 				t.Error(err)
@@ -127,11 +141,46 @@ func TestPutBin(t *testing.T) {
 			t.Error(err)
 			}
 
-	if testDataPut.Bins[0].Name != dataGet.Record.Bins[0].Name {
+	if testDataPut.Bins[0].Id != dataGet.Record.Bins[0].Id {
 		t.Errorf("Ожидалось %v, получили %s", testDataPut.Bins[0].Name, dataGet.Record.Bins[0].Name)
 	}
 
 	t.Cleanup(func() {
 		apimanager.Delete(id.Metadata.ID, key)
 	})
+}
+
+
+func TestDeleteBin(t *testing.T) {
+		err := godotenv.Load()
+			if err != nil {
+				t.Error("Не удалось найти ENV файл")
+			}
+	var apimanager api.ApiManager = &api.ApiStruct{}
+	key := apimanager.GetKey()
+	data, err := json.Marshal(testData)
+			if err != nil {
+				t.Error(err)
+			}
+			if err != nil {
+				t.Error(err)
+			}
+	id, err := apimanager.Post(data)
+			if err != nil {
+			t.Error(err)
+			}
+			if id == nil {
+				t.Errorf("ожидалась длина ответа больше 0, получили %s", id)
+			}
+	
+	err = apimanager.Delete(id.Metadata.ID, key)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_ , err = apimanager.Get(id.Metadata.ID, key)
+
+		if err == nil {
+			t.Errorf("Ожидалась ошибка, а получили данные")
+		}
 }
